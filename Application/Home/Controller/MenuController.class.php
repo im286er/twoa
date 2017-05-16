67b1f29d701ed04a4a233412f566db6f2be60ef1
vition <369709991@qq.com>
2017-05-16 18:53:56 +0800

    menu/profile

--- Application/Home/Controller/MenuController.class.php
+++ Application/Home/Controller/MenuController.class.php
@@ -2,7 +2,12 @@
 namespace Home\Controller;
 use Think\Controller;
 class MenuController extends AmangController {
+	//menu
 	function index(){
 		$this->display("menu");
+	}
+	//个人信息
+	function profile(){
+		$this->display("profile");
 	}
 }
