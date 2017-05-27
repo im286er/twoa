03ff4ff872cc0d1aa6d271938e58c1501a48ecb5
vition <369709991@qq.com>
2017-05-27 18:23:28 +0800

    ubase设置

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -1,36 +1,46 @@
 <?php
-/*用户功能{list|用户列表,create|新建用户,edit|编辑用户}*/
+/*用户功能{list|用户列表,create|新建用户,edit|编辑用户,ubase|基础信息}*/
 namespace Home\Controller;
 use Common\Controller\AmangController;
 class UserController extends AmangController {
 	//重组gethtml方法
 	public function gethtml(){
-		if (I("html")=='list'){
-			$user=M("oa_user u");
-			$userData=$user->field("user_name,user_code,c.config_value user_company,g.group_name user_group,p.place_name user_place,r.role_name user_role,user_higher,user_phone,user_avatar,user_born,user_sex,user_lastlogin,user_entry,user_login,user_state")->where("u.user_company=c.config_key AND u.user_group=g.group_id AND u.user_place=p.place_id AND u.user_role=r.role_id AND c.config_class='company'")->join("oa_config c,oa_group g,oa_place p,oa_role r")->select();
-			$this->assign("userlist",$userData);
-		}else if(I("html")=='create'){
-			$user=M("oa_user u");
-			$userData=$user->field("user_code")->order("user_code DESC")->find();
-			$this->assign("user_code",$userData["user_code"]+1);
-			$config=M("oa_config");
-			$companyData=$config->field("config_key,config_value")->where("config_class='company'")->select();
-			$this->assign("user_companys",$companyData);
+		switch (I("html")) {
+			case 'list':
+				$user=M("oa_user u");
+				$userData=$user->field("user_name,user_code,c.config_value user_company,g.group_name user_group,p.place_name user_place,r.role_name user_role,user_higher,user_phone,user_avatar,user_born,user_sex,user_lastlogin,user_entry,user_login,user_state")->where("u.user_company=c.config_key AND u.user_group=g.group_id AND u.user_place=p.place_id AND u.user_role=r.role_id AND c.config_class='company'")->join("oa_config c,oa_group g,oa_place p,oa_role r")->select();
+				$this->assign("userlist",$userData);
+				break;
+			case 'create': case 'ubase':
+				$user=M("oa_user u");
+				$userData=$user->field("user_code")->order("user_code DESC")->find();
+				$this->assign("user_code",$userData["user_code"]+1);
+				$config=M("oa_config");
+				$companyData=$config->field("config_key,config_value")->where("config_class='company'")->select();
+				$this->assign("user_companys",$companyData);
 
-			$group=M("oa_group g");
-			$groupData=$group->field("group_id,group_name")->select();
-			$this->assign("user_groups",$groupData);
+				$group=M("oa_group g");
+				$groupData=$group->field("group_id,group_name")->select();
+				$this->assign("user_groups",$groupData);
 
-			$place=M("oa_place");
-			$placeData=$place->field("place_id,place_name")->select();
-			$this->assign("user_place",$placeData);
+				$place=M("oa_place");
+				$placeData=$place->field("place_id,place_name")->select();
+				$this->assign("user_place",$placeData);
 
-			$role=M("oa_role");
-			$roleData=$role->field("role_id,role_name")->order("role_id ASC")->select();
-			$this->assign("user_roles",$roleData);
-
-		}else if(I("html")=='edit'){
-
+				$role=M("oa_role");
+				$roleData=$role->field("role_id,role_name")->where("role_upper=0")->order("role_id ASC")->select();
+				$this->assign("user_roles",$roleData);
+				break;
+			case 'edit':
+				# code...
+				break;
+			case 'ubases':
+				$config=M("oa_config");
+				$companyData=$config->field("config_key,config_value")->where("config_class='company'")->select();
+				$this->assign("user_companys",$companyData);
+				break;
+			default:
+				break;
 		}
 		parent::gethtml();
 	}
@@ -49,6 +59,23 @@ class UserController extends AmangController {
 			$result=$user->add($userData);
 			var_dump($result);
 		}
-		
 	} 
+	//
+	public function showlowe(){
+		if(IS_POST){
+			switch ($_POST['type']) {
+				case 'place':
+					$place=M('oa_place');
+					$placeData=$place->field("place_id,place_name")->where("place_group='{$_POST["id"]}'")->select();
+					# code...
+					break;
+				case 'role':
+					# code...
+					break;
+				default:
+					# code...
+					break;
+			}
+		}
+	}
 }
