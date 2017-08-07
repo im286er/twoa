38fd48f0f73dc5b50981aa478420f96d92d93277
vition <369709991@qq.com>
2017-08-07 18:21:10 +0800

    更新WeixinQy

--- ThinkPHP/Library/Vendor/WeixinQy/WeixinQy.php
+++ ThinkPHP/Library/Vendor/WeixinQy/WeixinQy.php
@@ -3,25 +3,25 @@
  * @Author: vition
  * @Date:   2017-08-02 09:45:11
  * @Last Modified by:   vition
- * @Last Modified time: 2017-08-07 15:57:32
+ * @Last Modified time: 2017-08-07 18:01:41
  */
 
 include_once "lib/Urllib.php";
 
 class WeixinQy extends Urllib{
 
-	protected $corpid;
-	protected $corpsecret;
-	protected $aTFile;
-	protected $accessToken;
+	protected $corpid;/*企业微信CorpID*/
+	protected $corpsecret;/*应用或者管理员secret*/
+	protected $aTFile;/*相关的accesstoken文件*/
+	protected $accessToken;/*储存access_token*/
 	protected $Jssdk;
-	protected $User;
-	protected $Message;
+	protected $User; /*实例化用户管理类*/
+	protected $Message;/*实例化消息管理类*/
 
 	/**
-	 * [__construct description]
-	 * @param [type] $corpid     [description]
-	 * @param [type] $corpsecret [description]
+	 * [__construct 构造方法]
+	 * @param [type] $corpid     [企业微信CorpID]
+	 * @param [type] $corpsecret [应用或者管理员secret]
 	 */
 	function __construct($corpid,$corpsecret){
 		$this->corpid=$corpid;
@@ -30,8 +30,11 @@ class WeixinQy extends Urllib{
 		$this->getToken();
 	}
 	
-	function getToken(){
-		
+	/**
+	 * [getToken 获取access token，缓存机制]
+	 * @return [type] [返回access token]
+	 */
+	private function getToken(){
 		if(file_exists($this->aTFile)){
 			$tokenData=json_decode(trim(substr($this->get($this->aTFile), 15)));
 			if ($tokenData->expire_time < time()) {
@@ -48,7 +51,8 @@ class WeixinQy extends Urllib{
 	}
 
 	/**
-	 * @return [type]
+	 * [createToken 生成access token]
+	 * @return [type] [返回access token]
 	 */
 	private function createToken(){
 		$accessTokenJson=$this->get("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={$this->corpid}&corpsecret={$this->corpsecret}");
@@ -57,14 +61,15 @@ class WeixinQy extends Urllib{
 			$aTFile = fopen($this->aTFile, "w");
 			fwrite($aTFile, "<?php exit();?>" . json_encode(array("expire_time"=>time() + 7000,"access_token"=>$ATObject->access_token)));
 			fclose($aTFile);
-			$tokenData=json_decode(trim(substr($this->get($this->aTFile), 15)));
-			print_r($tokenData);
-			return $tokenData->access_token;
+			return $ATObject->access_token;
 		}
 		return false;
 	}
 
-	/*用户管理*/
+	/**
+	 * [user 用户/部门管理]
+	 * @return [type] [返回一个对象]
+	 */
 	function user(){
 		if(!is_object($this->User)){
 			include_once "lib/User.php";
@@ -73,7 +78,10 @@ class WeixinQy extends Urllib{
 		return $this->User;
 
 	}
-	/*消息推送*/
+	/**
+	 * [message 消息管理类]
+	 * @return [type] [返回一个对象]
+	 */
 	function message(){
 		if(!is_object($this->Message)){
 			include_once "lib/Message.php";
@@ -83,21 +91,30 @@ class WeixinQy extends Urllib{
 	}
 
 	/**
-	 * @method   getAgent
-	 * @Author   vition
-	 * @DateTime 2017-08-02
-	 * @param    [type]
-	 * @return   [type]
+	 * [getAgent 获取应用]
+	 * @param  [type] $agentid [应用id]
+	 * @return [type]          [description]
 	 */
 	function getAgent($agentid){
 		return $this->get("https://qyapi.weixin.qq.com/cgi-bin/agent/get?access_token={$this->accessToken}&agentid={$agentid}");
 	}
 
-	/*网页授权 输出二维码*/
+	/**
+	 * [webLogin web授权登录 二维码]
+	 * @param  [type] $id      [要显示二维码的容器id（html）]
+	 * @param  [type] $appid   [企业微信的cropID]
+	 * @param  [type] $agentid [授权方的网页应用ID]
+	 * @param  [type] $url     [跳转的url]
+	 * @return [type]          [description]
+	 */
 	function webLogin($id,$appid,$agentid,$url){
-		echo '<script src="http://rescdn.qqmail.com/node/ww/wwopenmng/js/sso/wwLogin-1.0.0.js"></script><script> window.onload=function(){window.WwLogin({"id":"'.$id.'","appid" : "'.$appid.'","agentid" : "'.$agentid.'","redirect_uri" :"'.UrlEncode($url).'",});} </script>';
+		return '<script src="http://rescdn.qqmail.com/node/ww/wwopenmng/js/sso/wwLogin-1.0.0.js"></script><script> window.onload=function(){window.WwLogin({"id":"'.$id.'","appid" : "'.$appid.'","agentid" : "'.$agentid.'","redirect_uri" :"'.UrlEncode($url).'",});} </script>';
 	}
-	/*jss dk*/
+
+	/**
+	 * [jssdk 企业微信JS-SDK]
+	 * @return [type] [description]
+	 */
 	function jssdk(){
 		if(!is_object($this->Jssdk)){
 			include_once "lib/jssdk.php";
